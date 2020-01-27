5fea76b2ad0d122d66dc2b8318f1e8cddb898cfd
unknown <abdullrhmanaljasser@gmail.com>
2020-01-27 00:54:01 -0500

    -

--- LReg/initalCode.py
+++ LReg/initalCode.py
@@ -40,64 +40,59 @@ class LRegression():
                 counter += 1
             self._weights = np.array(newW)
         else: #Multiple traning example
+            N = tData[0].shape
+            
+            pred = self.predict(tData)
+            print(tData.shape)
+            grad = np.dot(tData.T, np.subtract(pred,corVec))
+            print(grad.shape)
+            grad /= N
+            #print(grad)
+            #print(tData[1])
+            grad *= lRate
+            
+            
+            self._weights = np.subtract(self._weights,grad)
+            print(self._weights)
             #tData = np.insert(tData,0,1,axis=1)
-            print(tData)
-            print(corVec)
-            counter = 0
-            for e in tData:
-                newW = []
-                innerC = 0
-                for w in self._weights:
-                    eSum = ((corVec[counter]-self.predict(e))*e[innerC])
-                    z = w + lRate*((corVec[counter]-self.predict(e*w))*e[innerC])
-                    newW.append(z)
-                    innerC += 1
-                self._weights = np.array(newW)
-                counter +=1
-        print(counter)
-        print(self._weights)
+#            print(tData)
+#            print(corVec)
+#            counter = 0
+#            for e in tData:
+#                newW = []
+#                innerC = 0
+#                
+#                for w in self._weights:
+#                    eSum = ((corVec[counter]-self.predict(e))*e[innerC])
+#                    z = w + lRate*((corVec[counter]-self.predict(e*w))*e[innerC])
+#                    newW.append(z)
+#                    innerC += 1
+#                self._weights = np.array(newW)
+#                counter +=1
+#        print(counter)
+#        print(self._weights)
 
 
     def predict(self,tSet):
         returnSet = []
-        
         if tSet.ndim == 1: #Single training example
-            power = 0
-            counter = 0
-            
-            for x in tSet:
-                
-                power += x * self._weights[counter]
-
-                counter +=1
-            #print(1.0/(1.0+np.exp(-power)))
-            returnSet.append(1/(1+np.exp(-power)))
+            rSet = np.dot(tSet,self._weights)
         else: #Set of inputs
-            for y in tSet:
-                power = 0
-                counter = 0
-                for x in y:
-                    
-                    power += x * self._weights[counter]
-                    counter +=1
-                returnSet.append(1/(1+np.exp(-power)))
-        if len(returnSet) > 1:
-            return np.array(returnSet)
-        else:
-            return returnSet[0]
+            rSet = np.dot(tSet,self._weights)
+        return self.sig(rSet)
+    
+    def sig(self,r):
+        return 1.0/(1.0+np.exp(-r))
         
 def main():
     df = np.array(pd.read_csv('data/sonar.all-data', sep=',',header=0))
-
+    
     x = LRegression(df[:,:60],df[:,60:],'R')
 
-    print(x.fit(x.featt,x.tarr,0.3))
+    for i in range(5000):
+        print(x.fit(x.featt,x.tarr,0.3))
     
-    for q in x.predict(x.featt):
-        if q > 1-q:
-            print("1")
-        else:
-            print("0")
+    
     
     
 
